a5bd7b47aea6bf0ba5add740aa971a3299d71541
Vishnupriya Rajendran <priyaraj968@outlook.com>
2023-07-08 12:55:02 +0530

    added remove method

--- QueuesDemo/PriorityQueueScratch.java
+++ QueuesDemo/PriorityQueueScratch.java
@@ -3,42 +3,50 @@ package DS.QueuesDemo;
 import java.util.Arrays;
 
 public class PriorityQueueScratch {
-    
+
     private int[] arrayitems;
     private int sizecount;
-    public PriorityQueueScratch(int capacity){
+
+    public PriorityQueueScratch(int capacity) {
         arrayitems = new int[capacity];
     }
 
     public void insert(int item) {
-        if(isEmpty()){
+        if (arrayitems.length == sizecount)
+            throw new IllegalStateException();
+
+        if (isEmpty()) {
             arrayitems[0] = item;
             sizecount++;
-        }
-        else{
-           for(int i= sizecount-1;i>=0;i--){// 1 
-                if(item<arrayitems[i])//3<5
+        } else {
+            for (int i = sizecount - 1; i >= 0; i--) {// 1
+                if (item < arrayitems[i])// 3<5
                 {
-                    arrayitems[i+1] = arrayitems[i];//a[1]=5
-                    arrayitems[i] = item;//a[0]=3
-                    sizecount++;//2
-                }   
-                else
-                {
-                    arrayitems[i+1] = item;
-                    sizecount++; 
+                    arrayitems[i + 1] = arrayitems[i];// a[1]=5
+                    arrayitems[i] = item;// a[0]=3
+                    sizecount++;// 2
+                } else {
+                    arrayitems[i + 1] = item;
+                    sizecount++;
                     break;
                 }
-            } 
-        }  
+            }
+        }
     }
+
+    public int remove() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return arrayitems[--sizecount];
+    } 
 
     public boolean isEmpty() {
         return sizecount == 0;
     }
 
     @Override
-    public String  toString() {
+    public String toString() {
         return (Arrays.toString(arrayitems));
     }
 }
