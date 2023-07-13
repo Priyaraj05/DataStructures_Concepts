946643c61bca47ae404e3184d30e90441640b351
Vishnupriya Rajendran <priyaraj968@outlook.com>
2023-07-12 18:14:58 -0700

    Revert "Revert "Add methods - find maximum value in Binary tree and Binary Search Tree""
    This reverts commit b6ae1d52ca9e13ae7615f2d7b3a933e7b9d42437.

--- HashTablesDemo/HashMain.java
+++ HashTablesDemo/HashMain.java
@@ -2,14 +2,17 @@ package DS.HashTablesDemo;
 
 public class HashMain {
     public static void main(String[] args) {
-        HashTableScratch hashTableScratch = new HashTableScratch();
-        hashTableScratch.put(123, "rosy");
-        hashTableScratch.put(1,"john");
-        hashTableScratch.put(123,"riya");
-        hashTableScratch.put(11,"joe");
-        System.out.println(hashTableScratch.get(123));
-        hashTableScratch.remove(1);
-        //hashTableScratch.remove(111); // throws illegal state exception
+        // HashTableScratch hashTableScratch = new HashTableScratch();
+        // hashTableScratch.put(123, "rosy");
+        // hashTableScratch.put(1,"john");
+        // hashTableScratch.put(123,"riya");
+        // hashTableScratch.put(11,"joe");
+        // System.out.println(hashTableScratch.get(123));
+        // hashTableScratch.remove(1);
+        // //hashTableScratch.remove(111); // throws illegal state exception
+        NonRepeatedFirstChar nrfc = new NonRepeatedFirstChar();
+        var ch = nrfc.NonRepCharFn("greeng");
+        System.out.println(ch);
         
     }
 }
