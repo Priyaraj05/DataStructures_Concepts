43c72052622d344bdf574d32ca1034c57735deaa
Vishnupriya Rajendran <priyaraj968@outlook.com>
2023-07-12 18:06:40 -0700

    Adding methods - find maximum value in Binary tree and Binary Search Tree

--- Trees/ScratchTree.java
+++ Trees/ScratchTree.java
@@ -1,8 +1,8 @@
 package DS.Trees;
 
+import java.security.Provider;
 
-
-public class Tree {
+public class ScratchTree {
 
     private Node root;
     
@@ -33,7 +33,7 @@ public class Tree {
 
         var current = root; 
         while (true) { 
-            if(item > current.value  ){ 
+            if(item > current.value){ 
                 if(current.rightChild == null){ 
                     current.rightChild = node; 
                     break;
@@ -67,10 +67,15 @@ public class Tree {
         return false;
     }
 
-    // overload to pass the private parameter root
+    // overload to pass the private parameter -  root
     public void PreOrdertraversal(){
         PreOrdertraversal(root);
     }
+    
+    /*         7
+     *   4          9
+     * 1    6    8      10
+    */
     private void PreOrdertraversal(Node root){
         // root left right
         if(root == null)
@@ -78,5 +83,85 @@ public class Tree {
         System.out.println(root.value);
         PreOrdertraversal(root.leftChild);
         PreOrdertraversal(root.rightChild);
+    }
+
+    public void InOrdertraversal(){
+        InOrdertraversal(root);
+    }
+    private void InOrdertraversal(Node root){
+        // Left root right
+        if(root == null)
+            return;
+
+        InOrdertraversal(root.leftChild);  
+        System.out.println(root.value);
+        InOrdertraversal(root.rightChild);
+        
+    }
+
+    public void PostOrdertraversal(){
+        PostOrdertraversal(root);
+    }
+    private void PostOrdertraversal(Node root){
+        // Left right root
+        if(root == null)
+            return;
+
+        PostOrdertraversal(root.leftChild);  
+        PostOrdertraversal(root.rightChild);
+        System.out.println(root.value);
+        
+    }
+
+    // height of the tree
+    public int HeightOfTree(){
+        int height = HeightOfTree(root);
+        return height;
+    }
+
+    private int HeightOfTree(Node root) {
+        if (root == null)
+            return -1;
+
+        if (isLeaf(root))
+            return 0;
+
+        return 1 + Math.max(HeightOfTree(root.leftChild),
+                HeightOfTree(root.rightChild));
+    }
+    // To find a maximum value in the Binary Search Tree-> O(log N)
+
+    public int MaxOfBST() {
+        return MaxOfBST(root);
+    }
+
+    private int MaxOfBST(Node root) {
+        var current = root;
+        var last = current;
+        while(current!= null){
+            last = current;
+            current = current.leftChild;
+        }
+        return last;
+    }
+    
+    
+    // To find a maximum value in the Binary Tree-> O(N)
+
+    public int MaxOfBinaryTree() {
+        return MaxOfBinaryTree(root);
+    }
+
+    private int MaxOfBinaryTree(Node root) {
+        if(isLeaf(root))
+            return root.value;
+
+        var leftMax = MaxOfBinaryTree(root.leftChild);
+        var rightMax = MaxOfBinaryTree(root.rightChild);
+        return Math.max(Math.max(leftMax, rightMax),root.value);
+    }
+
+    public boolean isLeaf(Node root) {
+        return root.leftChild == null && root.rightChild == null;
     }
 }
--- Trees/TreeMain.java
+++ Trees/TreeMain.java
@@ -2,17 +2,29 @@ package DS.Trees;
 
 public class TreeMain {
     public static void main(String[] args) {
-        Tree tree = new Tree();
+        ScratchTree tree = new ScratchTree();
         tree.insert(7);
         tree.insert(4);
         tree.insert(9);
         tree.insert(1);
         tree.insert(6);
         tree.insert(8);
-        tree.insert(10);        
+        tree.insert(10);
+        
         System.out.println("done");
         System.out.println(tree.find(7));
         System.out.println(tree.find(11));
+
+        System.out.println("Pre Order : ");
         tree.PreOrdertraversal();
+        System.out.println("In Order : ");
+        tree.InOrdertraversal();
+        System.out.println("Post Order : ");
+        tree.PostOrdertraversal();
+        
+        System.out.println("Height: " + tree.HeightOfTree());
+        System.out.println("Maximum value in tree: " + tree.MaxOfBinaryTree());
+        System.out.println("Maximum value: "+ tree.MaxOfBST());
+
     }
 }
